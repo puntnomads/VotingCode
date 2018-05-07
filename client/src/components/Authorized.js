deee6ea6527a37bbbcdf467b00d89945052a1655
puntnomads <puntnomads@gmail.com>
2018-05-07 12:01:48 +0100

    fixed module name error

--- client/src/components/Authorized.js
+++ client/src/components/Authorized.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
-import { checkUser } from "./Lib/checkAuth";
+import { checkUser } from "./Lib/checkUser";
 import { connect } from "react-redux";
 
 const AuthorizationHOC = WrappedComponent => {
